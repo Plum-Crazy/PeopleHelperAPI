536badca29314608344648b83c90fcbb9f3f3e2b
Mopar <webmaster@runelight.org>
2016-10-22 16:36:03 -0500

    Making things work

--- src/main/java/org/peoplehelper/rest/service/ClientService.java
+++ src/main/java/org/peoplehelper/rest/service/ClientService.java
@@ -1,13 +1,19 @@
 package org.peoplehelper.rest.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.peoplehelper.rest.database.ClientRepository;
 import org.peoplehelper.rest.database.model.ClientDBO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -17,6 +23,9 @@ public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
 
+    @Autowired
+    private ResourceLoader resourceLoader;
+
     @RequestMapping(value = "/getClientList", method = RequestMethod.GET)
     public List<ClientDBO> getClientList() {
         return (List<ClientDBO>) clientRepository.findAll();
@@ -25,6 +34,21 @@ public class ClientService {
     @RequestMapping(value = "/getClient", method = RequestMethod.GET)
     public ClientDBO getClientList(@RequestParam("id") long id) {
         return clientRepository.findByUuid(id);
+    }
+
+    @RequestMapping(value = "/convert", method = RequestMethod.GET)
+    public String convert() throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:data/Client.json");
+        InputStream stream = resource.getInputStream();
+
+        ObjectMapper mapper = new ObjectMapper();
+        ClientDBO[] list = mapper.readValue(stream, ClientDBO[].class);
+
+        clientRepository.deleteAll();
+
+        clientRepository.save(Arrays.asList(list));
+
+        return "success: " + list.length;
     }
 
 }
