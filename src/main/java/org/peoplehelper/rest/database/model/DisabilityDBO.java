7d7672e997f7cd360733185f2eae372fffed6348
Josh Fischer <josh@joshfischer.io>
2016-10-22 10:22:24 -0500

    broken

---
+++ src/main/java/org/peoplehelper/rest/database/model/DisabilityDBO.java
@@ -0,0 +1,62 @@
+package org.peoplehelper.rest.database.model;
+
+import javax.persistence.*;
+import javax.print.attribute.IntegerSyntax;
+import java.util.Date;
+
+@Entity
+@Table(name = "Disabilities")
+public class DisabilityDBO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "disability_primary_key")
+    private Long disabilityPrimaryKey;
+
+    @Column(name = "DisabilitiesId")
+    private String disabilitiesId;
+
+    @Column(name = "ProjectEntryID")
+    private Integer ProjectEntryId;
+
+    @Column(name = "PersonalID")
+    private Integer personalId;
+
+    @Column(name = "InformationDate")
+    private Date informationDate;
+
+    @Column(name = "DisabilityType")
+    private Integer disabilityType;
+
+    @Column(name = "DisabilityResponse")
+    private Boolean disabilityResponse;
+
+    @Column(name = "IndefiniteAndImpairs")
+    private Boolean indefiniteAndImpairs;
+
+    @Column(name = "DocumentationOnFile")
+    private Boolean documentationOnFile;
+
+    @Column(name = "ReceivingServices")
+    private Boolean recievingServices;
+
+    @Column(name = "InformationDate")
+    private Integer dataCollectionStage;
+
+    @Column(name = "InformationDate")
+    private Date dateCreated;
+
+    @Column(name = "InformationDate")
+    private Date dateUpdated;
+
+    @Column(name = "InformationDate")
+    private Long userId;
+
+    @Column(name = "InformationDate")
+    private Date dateDeleted;
+
+    @Column(name = "InformationDate")
+    private Integer exportId;
+
+
+}
