c6cf4f5b9c65597cf7f75dd4ceebf6434a4aecac
Mopar <webmaster@runelight.org>
2016-10-22 11:57:02 -0500

    Removing Spring Boot security and adding a getClientList endpoint that will return ALL clients in the database

--- src/main/java/org/peoplehelper/rest/service/ClientService.java
+++ src/main/java/org/peoplehelper/rest/service/ClientService.java
@@ -1,16 +1,24 @@
 package org.peoplehelper.rest.service;
 
+import org.peoplehelper.rest.database.ClientRepository;
+import org.peoplehelper.rest.database.model.ClientDBO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/client")
 public class ClientService {
 
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
-    public String testMe() {
-        return "hello world!";
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @RequestMapping(value = "/getClientList", method = RequestMethod.GET)
+    public List<ClientDBO> getClientList() {
+        return (List<ClientDBO>) clientRepository.findAll();
     }
 
 }
