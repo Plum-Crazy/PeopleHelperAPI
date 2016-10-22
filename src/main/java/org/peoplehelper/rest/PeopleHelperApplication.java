ebc69c4b79cf961e07a6f85bc1779bc30479c584
Mopar <webmaster@runelight.org>
2016-10-22 09:29:52 -0500

    Initial commit

---
+++ src/main/java/org/peoplehelper/rest/PeopleHelperApplication.java
@@ -0,0 +1,68 @@
+package org.peoplehelper.rest;
+
+import com.mysql.cj.jdbc.AbandonedConnectionCleanupThread;
+import org.apache.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+@SpringBootApplication
+public class PeopleHelperApplication {
+
+    private static final Logger LOG = Logger.getLogger(PeopleHelperApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeopleHelperApplication.class, args);
+    }
+
+    @Bean
+    protected ServletContextListener listener() {
+        return new ServletContextListener() {
+
+            @Override
+            public void contextInitialized(ServletContextEvent sce) {
+                LOG.info("Context initialized.");
+            }
+
+            @Override
+            public final void contextDestroyed(ServletContextEvent sce) {
+                LOG.info("Destroying context...");
+
+                try {
+                    LOG.info("Calling MySQL AbandonedConnectionCleanupThread shutdown.");
+                    AbandonedConnectionCleanupThread.shutdown();
+                } catch (InterruptedException e) {
+                    LOG.error("Error calling MySQL AbandonedConnectionCleanupThread shutdown.", e);
+                }
+
+                ClassLoader cl = Thread.currentThread().getContextClassLoader();
+
+                Enumeration<Driver> drivers = DriverManager.getDrivers();
+                while (drivers.hasMoreElements()) {
+                    Driver driver = drivers.nextElement();
+
+                    if (driver.getClass().getClassLoader() == cl) {
+
+                        try {
+                            LOG.info("Deregistering JDBC driver " + driver);
+                            DriverManager.deregisterDriver(driver);
+                        } catch (SQLException ex) {
+                            LOG.error("Error deregistering JDBC driver " + driver, ex);
+                        }
+
+                    } else {
+                        LOG.trace("Not deregistering JDBC driver " + driver + " as it does not belong to this webapp's ClassLoader");
+                    }
+                }
+            }
+        };
+    }
+
+}
---
+++ src/main/java/org/peoplehelper/rest/service/ClientService.java
@@ -0,0 +1,9 @@
+package org.peoplehelper.rest.service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/client")
+public class ClientService {
+}
